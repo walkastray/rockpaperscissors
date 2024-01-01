32f8816703a8d7e42293041e494dc20ffde32ddc
walkastray <47269745+walkastray@users.noreply.github.com>
2024-01-01 12:04:29 -0700

    Computer Choice Generator Function
    Function generates computer moves

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function playRound(){
-    
+function playRound(playerSelection, computerSelection){
+
 }
+
+/**
+ * Gets the computer's choice for rock paper scissors
+ * @returns string of rock, paper or scissors
+ */
+function getComputerChoice(){
+
+    let num = Math.floor(Math.random() * 3) + 1;
+    let choice;
+
+    switch(num){
+        case 1:
+            choice = "rock";
+        case 2:
+            choice = "paper";
+        case 3:
+            choice = "scissors";
+    }
+
+    return choice;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
